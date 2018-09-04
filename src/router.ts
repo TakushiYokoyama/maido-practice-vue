79c29fd696c1320cb8ba941884c03d226b63bad2
TakushiYokoyama <takushi.yokoyama@jointcrew.co.jp>
2018-09-04 23:38:14 +0900

    ディレクトリ構造を変更

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
-import completeIndex from '@/pages/complete/index.vue';
-import homeIndex from '@/pages/home/index.vue';
-import practiceIndex from '@/pages/practice/index.vue';
-import completeSlack from '@/pages/complete/slack.vue';
+import homeIndex from '@/index.vue';
+import completeIndex from '@/complete/pages/index.vue';
+import practiceIndex from '@/practice/pages/index.vue';
+import completeSlack from '@/complete/pages/slack.vue';
 import completeSlackChannel from '@/components/completeSlackChannel.vue';
 import Vue from 'vue';
 import Router from 'vue-router';
@@ -26,7 +26,7 @@ export default new Router({
       component: completeIndex,
     },
     {
-      path: '/complete-slack',
+      path: '/complete/slack',
       name: 'completeSlack',
       component: completeSlack,
       children: [
